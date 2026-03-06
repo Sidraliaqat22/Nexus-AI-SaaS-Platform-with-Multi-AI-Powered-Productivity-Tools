9ebd0be50f0530a2630740854a8fda9984da0a7f
Sidra Liaqat <seoul2003511@gmail.com>
2026-03-06 22:04:40 +0500

    added new

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { assets } from '../assets/assets.js'
 
 const Hero = () => {
 
@@ -22,6 +23,9 @@ const Hero = () => {
         hover:scale-102 active:scale-95 transition cursor-pointer'  > Start creating now</button>
         <button className='bg-white px-10 py-3 rounded-lg border 
         border-grey-300 hover:scale-102 active:scale-95 transition cursor-pointer' > Watch demo</button>
+      </div>
+      <div className='flex items-center gap-4 mt-8 mx-auto text-grey-600'>
+         <img src={assets.user_group} alt="" className='h-8'/> Trusted by 10k+ people
       </div>
           
 

e4ea56959902b89a55c32377d7ee5f21b2601442
Sidraliaqat22 <seoul2003511@gmail.com>
2026-05-30 22:17:00 +0500

    backend added

---
+++ server/controllers/userController.js
@@ -0,0 +1,62 @@
+// creat this file to publish the creations of user and also to show the history of creations
+import sql from '../configs/db.js';
+
+// function to publish the creation of user
+export const getUserCreations = async (req, res) => {
+    try{
+        const {userId}= req.auth()
+
+      const creations = await sql`SELECT* FROM creations WHERE user_id = ${userId} ORDER BY created_at DESC`;
+      res.json({success: true, creations});
+
+    }catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
+//  get published creations of all users fuction 
+export const getPublishedCreations = async (req, res) => {
+    try{
+        
+
+      const creations = await sql`SELECT* FROM creations WHERE publish = true ORDER BY created_at DESC`;
+      res.json({success: true, creations});
+
+    }catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
+//create a toggle function 
+export const toggleLikeCreation = async (req, res) => {
+    try{
+        
+      const{userId}= req.auth()
+      const {id} = req.body
+
+      const [creation]= await sql `SELECT* FROM creations WHERE id = ${id}`
+      if(!creation){
+        return res.json({success: false, message: "Creation not found"})
+      }
+      const currentLikes= creation.likes  ;
+      const userIdStr = userId.toString();
+      let updatedLikes;
+      let message;
+
+      if(currentLikes.includes(userIdStr)){
+        updatedLikes= currentLikes.filter((user)=> user !== userIdStr)
+        message = "Creation unliked"
+      } else {
+        updatedLikes= [...currentLikes, userIdStr]
+        message = "Creation Liked"
+      }
+
+      const formattedArray =`{${updatedLikes.join(',')}}`
+      await sql `UPDATE creations SET likes = ${formattedArray}: text[] WHERE id = ${id}`
+    
+
+
+      res.json({success: true, message});
+
+    }catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
